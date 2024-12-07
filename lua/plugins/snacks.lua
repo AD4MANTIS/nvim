d6f68722de5bfa661e888e1850e39e98e1eb463a
AD4MANTIS <31745687+AD4MANTIS@users.noreply.github.com>
2024-12-07 15:37:28 +0100

    Updated Dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,18 +2,14 @@ return {
   "folke/snacks.nvim",
   opts = {
     ---@class snacks.dashboard.Config
-    ---@field sections snacks.dashboard.Section
-    ---@field formats table<string, snacks.dashboard.Text|fun(item:snacks.dashboard.Item, ctx:snacks.dashboard.Format.ctx):snacks.dashboard.Text>
     dashboard = {
-      width = 60,
-      example = "files",
+      width = 70,
       sections = {
         { section = "header", enabled = false },
         function()
           local handle = io.popen("find ~/Pictures/Wallpaper -maxdepth 1 -type f | sort -R | head -n 1")
           local random_file = handle:read("*a"):gsub("%s+$", "") -- Trim trailing whitespace/newline
           handle:close()
-          print("Random file: " .. random_file)
           return {
             {
               section = "terminal",
@@ -33,17 +29,18 @@ return {
               -- chafa --format symbols --symbols braille --fg-only
               --
               -- Native Image
-              -- chafa --format iterm --size=1000x20
+              -- chafa --format iterm
               cmd = 'chafa "'
                 .. random_file
-                .. '" --format symbols --symbols ascii --fg-only --size=60x20 --align center; sleep .1',
-              height = 15,
+                .. '" --format symbols --symbols ascii --fg-only --size=70x20 --align center; sleep .1',
+              width = 70,
+              height = 20,
               padding = 1,
             },
           }
         end,
         { section = "keys", gap = 1, padding = 2 },
-        { icon = " ", title = "Projects", section = "projects", indent = 3, gap = 1, padding = 2 },
+        { icon = " ", title = "Projects", section = "projects", indent = 3, gap = 1, padding = 1 },
         { section = "session" },
         { section = "startup" },
       },
