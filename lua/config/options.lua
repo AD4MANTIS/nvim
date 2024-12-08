fae0fcd2167b729962c4d48fdb9d7b3040897ec0
AD4MANTIS <31745687+AD4MANTIS@users.noreply.github.com>
2024-12-08 23:46:46 +0100

    Centered Layout

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,6 +7,7 @@ vim.g.maplocalleader = " "
 
 local opt = vim.opt
 
+vim.o.numberwidth = 20 -- Give the code some left padding to center it a bit
 opt.linebreak = true -- Wrap lines at convenient points
 opt.scrolloff = 8 -- Lines of context
 opt.spelllang = { "en", "de" }
@@ -20,3 +21,7 @@ opt.virtualedit = "block" -- Allow cursor to move where there is no text in visu
 opt.termguicolors = true -- True color support
 
 -- opt.colorcolumn = "100"
+
+-- Enable the option to require a Prettier config file
+-- If no prettier config file is found, the formatter will not be used
+vim.g.lazyvim_prettier_needs_config = false
